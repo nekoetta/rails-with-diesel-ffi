27a9f28152129b095f4cb55573b9bf9a992dfafc
nekoetta <schwartzekatze1996510@gmail.com>
2022-06-28 19:45:45 +0900

    fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,13 +3,15 @@
 if [ ! -d "/usr/src/ffi/diesel_ffi/target" ]; then
     cd /usr/src/ffi/diesel_ffi
     $HOME/.cargo/bin/cargo build --release
+
+    cd /usr/src
+    bin/rails db:migrate;
+    bin/rails runner lib/rust_seed.rb;
 fi
 
 if [ ! -d "/usr/src/public/packs" ]; then
     cd /usr/src
     yarn install;
-    bin/rails db:migrate;
-    bin/rails runner lib/rust_seed.rb;
 fi
 
 exec $@
